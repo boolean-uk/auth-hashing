26603b8305bf98819dd70f1ae92e355508cefe2a
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-24 15:43:47 +0000

    still doing req 1

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const router = express.Router();
-
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const router = express.Router()
+secret = process.env.JWT_SECRET
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
-});
+  // Get the username and password from request body
+  const { username, password } = req.body
+  // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
 
-module.exports = router;
+  const saltRounds = 10
+
+  bcrypt
+
+  // Save the user using the prisma user model, setting their password to the hashed version
+
+  // Respond back to the client with the created users username and id
+  res.status(201).json({ user: undefined })
+})
+
+module.exports = router
