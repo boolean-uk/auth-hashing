9bcdabf88d89f57af4ed30b0a73693efbfe26a54
desokan <thefdmm@gmail.com>
2023-07-15 00:29:06 +0100

    completed

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
+router.post("/", async (req, res) => {
+  // Get the username and password from request body
+  const { username, password } = req.body;
+
+  // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+  const hashedPass = await bcrypt.hash(password, 10);
+  // Save the user using the prisma user model, setting their password to the hashed version
+  try {
+    const createdUser = await prisma.user.create({
+      data: { username, password: hashedPass },
+    });
+    // console.log("createdUser", createdUser);
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res
+      .status(201)
+      .json({ user: { id: createdUser.id, username: createdUser.username } });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(400)
+      .json({ message: "Error creating user: " + error.message });
+  }
 });
 
 module.exports = router;
