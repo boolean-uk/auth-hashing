f6b92f29fb8671fdac677c887111aee1ea67a775
Todd Griffin <todd.griffin61@outlook.com>
2023-07-14 11:35:20 +0100

    finished core 1

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -2,18 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+  const { username, password } = req.body
+
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    prisma.user.create({
+      data: {
+        username,
+        password: hash
+      }
+    })
+    .then((user) => {
+      return res.status(201).json({ user: { user: user.username, id: user.id } })
+    })
+  });
 });
 
 module.exports = router;
