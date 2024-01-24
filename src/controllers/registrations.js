02e825fa6afd9fe23bd95caf7fa9cff872d5385c
Satoki <satokiito@gmail.com>
2024-01-24 13:42:12 +0000

    response added when new user successfully created

--- src/controllers/registrations.js
+++ src/controllers/registrations.js
@@ -8,18 +8,16 @@ const registerUser = async (req, res) => {
     const { username, password} = req.body
 
     const userExists = await findUserDb(username)
-    if (userExists) return res.status(409).json({ error: "This username is already in use" })
+    if (userExists) return res.status(409).json({ error: "This username is already in use, please try a different username." })
 
     const hashedPassword = await bcrypt.hash(password, 12)
-
     try {
         const newUser = await createUserDb(username, hashedPassword)
-        console.log(newUser)
+        return res.status(201).json({ success: `Your registration is now complete, ${newUser.username}, please continue to login.`})
     }
     catch {
-        return res.status(500).json({ error: "Server error, please try again" })
+        return res.status(500).json({ error: "Server error, please try again." })
     }
-
 }
 
 module.exports = { registerUser }
