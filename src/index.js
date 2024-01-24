b7b231a837f6bb9eabf471437990eaae955fc192
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-24 18:14:34 +0000

    feat: add morgan

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
 const app = express();
 
 app.disable('x-powered-by');
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
