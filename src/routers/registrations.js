020fc27bf435f11257ed3d47d23504d12c73de4f
Elizabeth <lizbeepmc@gmail.com>
2024-01-24 19:02:55 +0300

    user registration

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+const { createUserRegistrationDb } = require("../domain/UserDB.js");
+
+
+router.post("/", async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const userInfo = await createUserRegistrationDb(username, hashedPassword);
+      res.status(201).json({ user: userInfo });
+    } catch (error) {
+      console.error("Error during user registration:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  
+
+  // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+
+  // Save the user using the prisma user model, setting their password to the hashed version
+
+  // Respond back to the client with the created users username and id
 });
 
 module.exports = router;
