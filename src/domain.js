72d13b97f32e4af4e0204e972a811d73af3a9b37
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-25 10:05:41 +0100

    helper functions (utils.js)

--- src/domain.js
+++ src/domain.js
@@ -1,18 +1,36 @@
+const { hashString, saltString, compareStrings } = require('./utils')
+
 const registerUserDb = async (username, password) => {
 
 }
 
 const loginUserDb = async (username, password) => {
   const getUser = getUserDb(username)
-  if (pass)
+
+  if (!getUser) res.status(409).json({ error: 'username or password incorrect' })
+
+  const hashedPassword = hashString(password)
+
+  try {
+    
+  } catch (error) {
+
+  }
 }
 
 const getUserDb = async (username) => {
-  const foundUser = await prisma.user.findUniqueOrThrow({ 
-    where: {
-      username
-    }
-  })
+
+  try {
+    const foundUser = await prisma.user.findUniqueOrThrow({ 
+      where: {
+        username
+      }
+    })
+    
+    return foundUser
+  } catch (error) {
+
+  }
 }
 
 module.exports = { registerUserDb, loginUserDb }
